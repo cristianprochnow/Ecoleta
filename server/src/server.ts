7891c31aa731bdf66bac46ddee34c71dc7d1ea21
cristianprochnow <cristianprochnow@gmail.com>
2020-06-03 23:53:19 -0300

    front-end of the application

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,9 +5,9 @@ import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use(cors())
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
