f771c042d080cca24bfb3cb2fa6b68f39d7f2d3a
cristianprochnow <cristianprochnow@gmail.com>
2020-06-17 11:02:05 -0300

    chore(server): add test setup

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express, { request, response } from 'express'
+import cors from 'cors'
+import path from 'path'
+import { errors } from 'celebrate'
+
+import routes from './routes'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(errors())
+
+export default app
