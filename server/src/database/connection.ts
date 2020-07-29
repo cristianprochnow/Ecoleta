31873b32a6480b397876ec23f90b2cc5485523f9
Cristian Prochnow <cristianprochnow@gmail.com>
2020-07-28 21:33:20 -0300

    Update connection.ts

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -3,7 +3,7 @@ import path from 'path'
 
 const knexConfig = require('../../knexfile')
 
-const config  = process.env.NODE_ENV === 'test' ? knexConfig.test : knexConfig.test
+const config  = process.env.NODE_ENV === 'test' ? knexConfig.test : knexConfig.development
 
 const connection = knex(config)
 
